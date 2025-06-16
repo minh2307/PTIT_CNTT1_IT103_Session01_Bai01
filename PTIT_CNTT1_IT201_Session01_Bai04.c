20fec0214b86a3f9af809b4422a2a31ef6fa6eeb
minh2307 <minh23923@gmail.com>
2025-06-16 22:22:54 +0700

    fix

--- PTIT_CNTT1_IT201_Session01_Bai04.c
+++ PTIT_CNTT1_IT201_Session01_Bai04.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
-int sum (int sum){
+int main (){ 
+    int n=10;
+    int sum1=0,sum2=0;
+// cách 1:
+// time complexcity: o(n)
+// space complexcity: o(1)    
+ 
     for (int i = 0; i < n; i++){
         sum += i;
     }
-    return sum;
+
+// cách 2: 
+// time complexcity: O(1)
+// space complexcity: O(1)
+
+    sum = n*(n+1)/2 
 }
 
-// time complexcity: o(1)
-// space complexcity: o(1)
--- PTIT_CNTT1_IT201_Session01_Bai05.c
+++ PTIT_CNTT1_IT201_Session01_Bai05.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>  
 
-int bubbleSort(int temp){
+int main(){
+    // time complexity: O(n^2)
+    // space complexity: O(1)
+
+    int temp;
     int* arr = (int*)malloc(100 * sizeof(int));
-    for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-        for (int j = 0; j < sizeof(arr)/sizeof(arr[0]) - 1 - i; j++){
-            if(a[j] > a[j+1]){
-                temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
+    int n = sizeof(arr)/sizeof(arr[0]);  
+
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n - 1 - i; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }        
     }
-}
 
-// time complexcity: O(n^2)
-// space complexcity: O(1)
+    printf("Mang sau khi sap xep: ");
+    for (int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    return 0;
+}
